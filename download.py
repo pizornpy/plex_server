de5dea355266036b782d47782bcf3e9023b20a3f
Juan <juanjulionn@gmail.com>
2025-05-03 19:36:27 +0200

    first commit with git ignore

---
+++ download.py
@@ -0,0 +1,28 @@
+from torrentp import TorrentDownloader
+import asyncio
+import os
+import re
+import time
+from func import *
+
+# Directory where files are downloaded
+file_location = r"/mnt/c/Users/Juan/plex/media/tvshows"
+
+# Download the torrent file
+torrent_file = TorrentDownloader(
+    "magnet:?xt=urn:btih:58255314E9A12352EC0A0AB643A13C6CDD1819BC&dn=Arcane%20S02E01%201080p%20WEB%20H264-SuccessfulCrab&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce",
+    file_location
+)
+
+# Start the download
+asyncio.run(torrent_file.start_download())
+
+time.sleep(120)  
+
+# Get the last downloaded file
+last_downloaded_file = FlexOp.get_last_downloaded_file(file_location)
+if last_downloaded_file:
+    print(f"Last downloaded file: {last_downloaded_file}")
+    FlexOp.rename_for_plex(last_downloaded_file)
+else:
+    print("No files found in the directory.")
---
+++ func.py
@@ -0,0 +1,44 @@
+from torrentp import TorrentDownloader
+import asyncio
+import os
+import re
+import time
+
+class FlexOp:
+    # Function to rename files for Plex
+    @staticmethod
+    def rename_for_plex(file_path):
+        # Extract information from the file name using regex
+        match = re.search(r"(.*?)S(\d{2})E(\d{2})", file_path, re.IGNORECASE)
+        if not match:
+            print("File name does not match the expected format.")
+            return
+
+        show_name = match.group(1).replace(".", " ").strip()
+        season_num = match.group(2)
+        episode_num = match.group(3)
+
+        # Create the new file name
+        new_name = f"{show_name} - S{season_num}E{episode_num}{os.path.splitext(file_path)[1]}"
+        new_path = os.path.join(os.path.dirname(file_path), new_name)
+
+        # Rename the file
+        os.rename(file_path, new_path)
+        print(f"Renamed '{file_path}' to '{new_path}'")
+
+    # Function to get the most recently modified file in a directory
+    @staticmethod
+    def get_last_downloaded_file(directory):
+        files = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+        if not files:
+            return None
+        # Sort files by modification time
+        files.sort(key=os.path.getmtime, reverse=True)
+        return files[0]  # Return the most recent file
+
+    # Function to download a torrent
+    @staticmethod
+    async def download(magnet, path):
+        torrent_file = TorrentDownloader(magnet, path)
+        await torrent_file.start_download()
+        return torrent_file
