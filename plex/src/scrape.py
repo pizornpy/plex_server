bc08d382401ced3200d874024d823daae12a1d50
pizornpy <juanjulionn@gmail.com>
2025-07-05 13:29:39 +0200

    added integration with 1337 torrent

--- plex/src/scrape.py
+++ plex/src/scrape.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import beautifulsoup4
+from bs4 import BeautifulSoup
 import requests
 
 
@@ -9,7 +9,19 @@ load_dotenv()
 class Scrape_utils(): 
 
     def make_url(movie): 
-        return f"{torrent_site}/srch?search={movie}"
+        return f"{torrent_site}/srch?search={movie}/"
 
-    def get_page(url): 
-        response 
+    def get_magnet_links(search_query):
+        base_url = f"https://1337x.to/search/{search_query}/1/"
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(base_url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        links = soup.select('a[href^="/torrent/"]')
+        torrent_page = "https://1337x.to" + links[0]['href']
+
+        page = requests.get(torrent_page, headers=headers)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        magnet_link = soup.select_one('a[href^="magnet:"]')['href']
+
+        return magnet_link
